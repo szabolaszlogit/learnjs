8868b4415eb0c4d8075fa6dfa0d37d9061bbfd35
szab9laszlogit <you@example.comszabolaszlo.mail@gmail.com>
2022-11-13 18:05:50 +0100

    icons

--- src/main.js
+++ src/main.js
@@ -3,7 +3,28 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { faJs } from "@fortawesome/free-brands-svg-icons";
+import { faVuejs } from "@fortawesome/free-brands-svg-icons";
+import { faNodeJs } from "@fortawesome/free-brands-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faUserSecret,
+  faJs,
+  faVuejs,
+  faNodeJs,
+  faDatabase,
+  faUpRightFromSquare
+);
+
 const app = createApp(App);
 
+app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(router);
 app.mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,41 @@
-<script setup></script>
+<script setup>
+import { reactive } from "vue";
+
+const plans = reactive([
+  { icon: "fa-brands fa-vuejs", txt: "Vue" },
+  { icon: "fa-brands fa-node-js", txt: "Node" },
+  { icon: "fa-solid fa-database", txt: "MongoDB" },
+]);
+</script>
 
 <template>
   <article>
-    <h1>Ismeretek a JavaScript nyelvről</h1>
+    <h1>
+      Ismeretek a <font-awesome-icon icon="fa-brands fa-js " /> JavaScript
+      nyelvről
+    </h1>
     <p>A tartalom bővül..</p>
     <p>Tervek:</p>
     <ul>
-      <li>Vue</li>
-      <li>Node - Express</li>
-      <li>MongoDb</li>
+      <li v-for="plan in plans">
+        <font-awesome-icon :icon="plan.icon" class="icon" />{{ plan.txt }}
+      </li>
     </ul>
+<hr>
     <div>
       <a href="https://github.com/szabolaszlogit/learnjs" target="_blank"
-        >Ez az oldal a githubon</a
+        >Ez az oldal a githubon <font-awesome-icon icon="fa-solid fa-up-right-from-square" /></a
       >
     </div>
   </article>
 </template>
+
+<style scoped>
+h1 {
+  font-size: 1.4rem;
+}
+.icon {
+  margin-right: 1rem;
+  color: blue;
+}
+</style>
